66b4ea5c32599781442e6a30224520da66016ef5
sajid <reznov9185@gmail.com>
2016-06-12 01:40:59 +0600

    Clean blog themed rails app - Theme added and basic setup completed

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  root 'clean_blogs#index'
+  get  'about'    => 'clean_blogs#about'
+  get  'post'     => 'clean_blogs#post'
+  get  'contact'  => 'clean_blogs#contact'
+
+end
